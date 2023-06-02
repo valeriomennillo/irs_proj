35359ef1dc2faef0b944b58125966a45fac3bef9
vee <theplayer271@gmail.com>
2023-06-02 21:13:14 +0200

    model class construction start

--- buildplug.cmd
+++ buildplug.cmd
@@ -1,4 +1,6 @@
-cd solr_plugin
+cd solr_plugin\my-solr-plugin
 mvn clean install
-copy target\CustomHandler-1.jar ..\solr-9.2.1\lib
+cd ..
+copy target\CustomHandler-1.jar ..\..\solr-9.2.1\lib
+cd ..
 ..\solr-9.2.1\bin\solr.cmd start -f 
---
+++ solr_plugin/my-solr-plugin/src/main/java/me/val/plugins/CNN_Model.java
@@ -0,0 +1,55 @@
+package me.val.plugins;
+
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.Tensors;
+import java.io.File;
+
+public class CNN_Model {
+    private Session session;
+
+    public void init(String modelFilePath) {
+        try {
+            byte[] modelData = readModelData(modelFilePath);
+            Graph graph = new Graph();
+            graph.importGraphDef(modelData);
+            session = new Session(graph);
+            System.out.println("Model initialized successfully.");
+        } catch (Exception e) {
+            System.err.println("Failed to initialize the model: " + e.getMessage());
+        }
+    }
+
+    public float[] calculate(String imagePath) {
+        try {
+            float[] featureVector = preprocessImage(imagePath);
+            Tensor<Float> inputTensor = Tensors.create(featureVector);
+            Tensor<Float> outputTensor = session.runner().feed("input", inputTensor)
+                                                       .fetch("output")
+                                                       .run()
+                                                       .get(0)
+                                                       .expect(Float.class);
+            float[] output = outputTensor.copyTo(new float[outputTensor.shape()[0]]);
+            inputTensor.close();
+            outputTensor.close();
+            return output;
+        } catch (Exception e) {
+            System.err.println("Failed to calculate feature vector: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private byte[] readModelData(String modelFilePath) throws Exception {
+        File modelFile = new File(modelFilePath);
+        // Implement the logic to read the model file data
+        // and return it as a byte array
+        return null;
+    }
+
+    private float[] preprocessImage(String imagePath) {
+        // Implement the logic to preprocess the image
+        // and return the feature vector
+        return null;
+    }
+}

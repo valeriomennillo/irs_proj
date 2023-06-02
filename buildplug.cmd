e14d71b82ccdeff82aa0f47a0fd355de439f25e8
vee <theplayer271@gmail.com>
2023-06-02 18:06:34 +0200

    t

--- buildplug.cmd
+++ buildplug.cmd
@@ -1,4 +1,5 @@
 cd solr_class
 javac -classpath solr-core-9.2.1.jar;solr-solrj-9.2.1.jar;solr-solrj-streaming-9.2.1.jar;solr-solrj-zookeeper-9.2.1.jar CustomRequestHandler.java
 jar cvf custom-handler.jar CustomRequestHandler.class
-..\..\solr-9.2.1\bin\solr.cmd start -f 
+cd ..
+..\solr-9.2.1\bin\solr.cmd start -f 

9820c6dea81d9cc8ad5906b7c5e582576253e10d
vee <theplayer271@gmail.com>
2023-06-04 16:44:02 +0200

    solr upd

--- compile.bat
+++
@@ -1,4 +0,0 @@
-cd solr_class
-javac -classpath libs/* CustomRequestHandler.java
-jar cvf CustomHandler.jar CustomRequestHandler.class
-cd ..
--- summary.py
+++
@@ -1,4 +0,0 @@
-from tensorflow.keras.applications import VGG16
-
-base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-base_model.summary()
